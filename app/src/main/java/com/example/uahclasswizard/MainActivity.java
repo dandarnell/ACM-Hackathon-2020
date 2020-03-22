07ad81743a6285db7d97f5f6bcb61064110ebb7b
dandarnell <dandarnell@users.noreply.github.com>
2020-03-22 11:55:37 -0500

    Updated layout

--- app/src/main/java/com/example/uahclasswizard/MainActivity.java
+++ app/src/main/java/com/example/uahclasswizard/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.uahclasswizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -13,22 +18,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView searchOutput;
-
     private List<UAHClass> uahClasses = new ArrayList<UAHClass>();
 
-    private List<String> buildings = new ArrayList<>();
+    private List<String> departments = new ArrayList<String>();
+    ArrayAdapter<String> departmentAdapter;
+
+    private List<String> buildings = new ArrayList<String>();
+    ArrayAdapter<String> buildingAdapter;
+
+    private List<String> instructors = new ArrayList<String>();
+    ArrayAdapter<String> instructorAdapter;
+
+    Spinner buildingSpinner;
+    Button listByBuildingButton;
+
+    Spinner roomBuildingSpinner;
+    EditText roomNumberEdit;
+    Button listSemesterByRoomButton;
+    Button listByRoomButton;
+
+    TextView searchOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchOutput = findViewById(R.id.searchOutput);
+        initializeUAHClasses();
+        initializeUI();
+    }
 
+    private void initializeUAHClasses() {
         InputStream inputStream = this.getResources().openRawResource(R.raw.uah_classes);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
 
         String line = null;
 
@@ -46,16 +68,78 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if(line != null) {
-                uahClasses.add(new UAHClass(line));
+                UAHClass uahClass = new UAHClass(line);
+
+                uahClasses.add(uahClass);
+
+                if(!departments.contains(uahClass.getDepartment())) {
+                    departments.add(uahClass.getDepartment());
+                }
+
+                if(!buildings.contains(uahClass.getBuilding())) {
+                    buildings.add(uahClass.getBuilding());
+                }
+
+                if(!instructors.contains(uahClass.getInstructor())) {
+                    instructors.add(uahClass.getInstructor());
+                }
             } else {
                 break;
             }
         }
-
-        searchOutput.setText(uahClasses.get(uahClasses.size()-1).toString());
     }
 
-    private void initializeButtons() {
+    private void initializeUI() {
+        buildingAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, buildings);
+        buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        buildingSpinner = findViewById(R.id.buildingSpinner);
+        buildingSpinner.setAdapter(buildingAdapter);
+
+        listByBuildingButton = findViewById(R.id.listByBuildingButton);
+        listByBuildingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String building = buildingSpinner.getSelectedItem().toString();
+                String results = new String();
+
+                for(UAHClass uahClass : uahClasses) {
+                    if(uahClass.isBuilding(building)) {
+                        results += "\n\n" + uahClass.toString();
+                    }
+                }
+
+                searchOutput.setText(results);
+            }
+        });
+
+        roomBuildingSpinner = findViewById(R.id.roomBuildingSpinner);
+        roomBuildingSpinner.setAdapter(buildingAdapter);
+
+        roomNumberEdit = findViewById(R.id.roomNumberEdit);
+
+        listSemesterByRoomButton = findViewById(R.id.listSemesterByRoomButton);
+
+        listByRoomButton = findViewById(R.id.listByRoomButton);
+        listByRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String building = roomBuildingSpinner.getSelectedItem().toString();
+                String results = new String();
+
+                for(UAHClass uahClass : uahClasses) {
+                    if(uahClass.isRoom(building,roomNumberEdit.getText().toString())) {
+                        results += "\n\n" + uahClass.toString();
+                    }
+                }
+
+                searchOutput.setText(results);
+            }
+        });
+
+        searchOutput = findViewById(R.id.searchOutput);
     }
 }
