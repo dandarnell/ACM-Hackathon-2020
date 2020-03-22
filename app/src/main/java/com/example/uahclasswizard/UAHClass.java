5eecea771a54b8942910fd89a8307b0c8d9d863e
Andrew Clark <ac8774@gmail.com>
2020-03-22 11:14:28 -0400

    untested but probably works (?)

--- app/src/main/java/com/example/uahclasswizard/UAHClass.java
+++ app/src/main/java/com/example/uahclasswizard/UAHClass.java
@@ -1,7 +1,48 @@
 package com.example.uahclasswizard;
 
 public class UAHClass {
-    UAHClass(String csvLine) {
 
+    // name,crn,department,number,section,term,credits,start time,end time,days,start date,end date,building,room number,instructor
+
+    String name;
+    String crn;
+    String department;
+    String number;
+    String section;
+    String term;
+    String credits;
+    String startTime;
+    String endTime;
+    String days;
+    String startDate;
+    String endDate;
+    String building;
+    String roomNumber;
+    String instructor;
+
+    UAHClass(String csvLine) {
+        String[] things = csvLine.split(",");
+        this.name = things[0];
+        this.crn = things[1];
+        this.department = things[2];
+        this.number = things[3];
+        this.section = things[4];
+        this.term = things[5];
+        this.credits = things[6];
+        this.startTime = things[7];
+        this.endTime = things[8];
+        this.days = things[9];
+        this.startDate = things[10];
+        this.endDate = things[11];
+        this.building = things[12];
+        this.roomNumber = things[13];
+        this.instructor = things[14];
+    }
+
+    public String toString() {
+        return this.name + " - " + this.department + " " + this.number + " - " + this.section
+                + "\n  " + this.days + " " + this.startTime + " - " + this.endTime
+                + "\n  " + this.building + " " + this.roomNumber
+                + "\n  " + this.instructor + "\n";
     }
 }
